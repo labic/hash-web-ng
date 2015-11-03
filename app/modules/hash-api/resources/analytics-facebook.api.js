731b3062c45545f17f7028857c13ea530491d58e
GustavoRPS <email@gustavorps.net>
2015-11-03 19:53:08 -0200

    bug fix resource service expects a response that can be deseriali

--- app/modules/hash-api/resources/analytics-facebook.api.js
+++ app/modules/hash-api/resources/analytics-facebook.api.js
@@ -18,24 +18,28 @@
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/facebook/most_liked_posts', 
           params: PARAMS,
+          isArray: true,
           cache: true
         },
         mostSharedPosts: {
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/facebook/most_shared_posts', 
           params: PARAMS,
+          isArray: true,
           cache: true
         },
         mostCommentedPosts: {
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/facebook/most_commented_posts', 
           params: PARAMS,
+          isArray: true,
           cache: true
         },
         mostCommentedPosts: {
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/facebook/most_active_profiles', 
           params: PARAMS,
+          isArray: true,
           cache: true
         }
       });
--- app/modules/hash-api/resources/analytics-twitter.api.js
+++ app/modules/hash-api/resources/analytics-twitter.api.js
@@ -19,42 +19,49 @@
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/twitter/most_retweeted_tweets', 
           params: PARAMS,
+          isArray: true,
           cache: true
         },
         mostMentionedUsers: {
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/twitter/most_mentioned_users', 
           params: PARAMS,
+          isArray: true,
           cache: true
         },
         mostRetweetedUrls: {
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/twitter/most_retweeted_urls', 
           params: PARAMS,
+          isArray: true,
           cache: true
         },
         mostRetweetedImages: {
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/twitter/most_retweeted_images', 
           params: PARAMS,
+          isArray: true,
           cache: true
         },
         mostActiveUsers: {
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/twitter/most_active_users', 
           params: PARAMS,
+          isArray: true,
           cache: true
         },
         mostPopularHashtags: {
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/twitter/most_popular_hashtags', 
           params: PARAMS,
+          isArray: true,
           cache: true
         },
         geolocation: {
           method: 'GET', 
           url:  HASH_API_BASE_URI + '/analytics/twitter/geolocation', 
           params: PARAMS,
+          isArray: true,
           cache: true
         }
       });
