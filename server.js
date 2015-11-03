ff5b4e38235fc8d4f1edb53bde0f1b915303e91e
zoaldyck <roberto_dmoraes@hotmail.com>
2015-11-03 18:18:50 -0200

    Clening code and ajust directories

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express  = require('express');
 var app      = express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname));
 
 app.listen(8080);
 
 app.get('/', function (req, res) {
-  res.sendfile('./app/index.html');
+  res.sendfile('./index.html');
 });
 
 console.log("App listening on port 8080");
