5c7ee30ab18796a5adce957effc936e9b8e3b630
GustavoRPS <email@gustavorps.net>
2015-11-03 17:39:44 -0200

    Removed wrong entered directory

--- labic-sdh-front-end-bcbf1da6949d/app/modules/twitter-monitor/config/twitter-monitor.config.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-  angular
-    .module('hash.twitter-monitor')
-    .run(function ($rootScope) {
-      $rootScope.addModuleMenu('Twitter', 'app.twitter-monitor.view', 'fa-edit');
-    });
-
-})();
--- labic-sdh-front-end-bcbf1da6949d/app/modules/twitter-monitor/hash.twitter-monitor.js
+++
@@ -1,4 +0,0 @@
-(function () {
-  'use strict';
-  angular.module('hash.twitter-monitor', ['hash.api']);
-})();
