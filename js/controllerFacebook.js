2bcaa9e90cac51a4c27b55d5a0933b41a2411c7f
zoaldyck <roberto_dmoraes@hotmail.com>
2015-11-19 14:28:40 -0200

    Testes com a API DO FACEBOOK

--- js/controllerFacebook.js
+++ js/controllerFacebook.js
@@ -5,9 +5,11 @@ hashTwitter.controller('mainFacebook', function ($scope, $http, MetricsFacebook,
     time: '7d',
     word: '',
     type: 'comentario',
-    actor: 'imprensa',
+    actor: null,
     categoria: null,
-    categorieNumber: 0
+    categorieNumber: 0,
+    page: 1,
+    per_page: 25
   };
 
   //  MetricsFacebook.count({
@@ -21,18 +23,27 @@ hashTwitter.controller('mainFacebook', function ($scope, $http, MetricsFacebook,
   $scope.$watch('filter', function (newFilter, oldFilter) {
 
     $scope.analyticsFacebookParams = {
-      period: newFilter.time, 
-      profile_type: newFilter.actor, 
-      'post_type[]': [newFilter.tema], 
-      page: null, 
-      per_page: null
+      //      period: newFilter.time, 
+      //      profile_type: newFilter.actor, 
+      //      'post_type[]': [newFilter.tema], 
+      //      page: null, 
+      //      per_page: null
+
+      profile_type: 'page', // Requiried
+      period: newFilter.time,
+      'filter[with_tags]': [newFilter.tema],
+      'filter[contain_tags]': [],
+      'filter[hashtags]': [],
+      'filter[profiles]': [],
+      'filter[mentions]': [],
+      'filter[type]': []
     };
 
     $scope.wordFacebookParams = {
       period: newFilter.time,
       tags: [newFilter.tema],      
-      page: null,    
-      per_page: null
+      page: 1,    
+      per_page: 20
     };
 
     $http.get("data/test/conteudos.json").success(function (data) {
@@ -47,11 +58,13 @@ hashTwitter.controller('mainFacebook', function ($scope, $http, MetricsFacebook,
 
     if(newFilter.type == 'comentario'){
 
-      AnalyticsFacebook.mostCommentedPosts(
-        $scope.analyticsFacebookParams, 
-        function success(response) {
-          $scope.posts = response;
-        }, errorHandler);
+      // falta o receber os comentários mais curtidos e não os posts mais comentados.
+
+      //      AnalyticsFacebook.mostCommentedPosts(
+      //        $scope.analyticsFacebookParams, 
+      //        function success(response) {
+      //          $scope.posts = response;
+      //        }, errorHandler);
 
     }else{
 
