e0c7ef14dcec6aeb05843a2029cbfedfd4a68b62
zoaldyck <roberto_dmoraes@hotmail.com>
2015-11-29 21:02:20 -0200

    facebook interface inicial completa inicio da emplementação dos requests.

--- js/controllerRelatorios.js
+++ js/controllerRelatorios.js
@@ -1,3 +1,18 @@
-hashTwitter.controller('mainRelatorio', function ($scope, $http, MetricsFacebook) {
-  
+hashTwitter.controller('mainRelatorio', function ($scope, $http, MetricsFacebook, MetricsTwitter, Tweet) {
+
+  Tweet.count({
+    period: "7d",
+    'filter[contain_tags]': ["tema-negros","tema-generos","tema-indigena","tema-lgbt"]
+  }, function success(res) {
+    $scope.countTweetTwitter = res.count;
+  });
+
+  Tweet.count({
+    period: "7d",
+    'filter[contain_tags]': ["tema-negros","tema-generos","tema-indigena","tema-lgbt"],
+    'filter[has]': ['media']
+  }, function success(res) {
+    $scope.countImageTwitter = res.count;
+  });
+
 });
