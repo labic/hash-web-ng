cd6a381d22a7e5a5435ef2cc0cafe46a613f2fa1
Gustavo RPS <email@gustavorps.net>
2016-03-02 16:45:01 -0300

    init implementation of gulp with browser-sync to generate automatic deploy

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var browserSync = require('browser-sync').create(),
+    gulp        = require('gulp');
+
+var paths = {
+  app:   './app'
+};
+
+gulp.task('watch', function() {
+  gulp.watch('app/**/*.html').on('change', browserSync.reload);
+  gulp.watch('app/**/*.js').on('change', browserSync.reload);
+});
+
+gulp.task('serve', ['watch'], function() {
+  browserSync.init({
+    server: paths.app
+  });
+});
+
+gulp.task('default', ['serve']);
