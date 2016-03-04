f9559fd8f83bd6572f769780c4c63f5360c002be
Gustavo RPS <email@gustavorps.net>
2016-03-04 15:08:18 -0300

    implemented period filter directive

---
+++ app/modules/twitter/directives/period-filter.js
@@ -0,0 +1,23 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('hash.twitter')
+    .directive('periodFilter', function () {
+      return {
+        templateUrl: 'modules/twitter/views/partials/period-filter.html',
+        restrict: 'E',
+        replace: true,
+        scope: {
+          options: '=',
+          filter: '='
+        },
+        link: function(scope, element, attrs) {
+          scope.setFilter = function(e) {
+            scope.filter = $(e.currentTarget).attr('value');
+          }
+        }
+      };
+    });
+
+})();
