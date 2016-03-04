195f003d4c95d1127498f8d8d6416dfac9793a40
Gustavo RPS <email@gustavorps.net>
2016-03-04 16:56:40 -0300

    cleaning some code

--- app/modules/instagram/controllers/main.js
+++ app/modules/instagram/controllers/main.js
@@ -1,8 +1,5 @@
 hash.controller('mainInstagram', function ($scope, $http) {
 
-  
-//  $(".fade_white").show();
-  
   $scope.filter = {
     tema: 'tema-negros',
     time: 'recent',
@@ -11,7 +8,7 @@ hash.controller('mainInstagram', function ($scope, $http) {
   $scope.$watch('filter', function (newFilter, oldFilter) {
     $(".loading_instagram").show();
     $(".fade_white").show();
-    
+
     var url = serviceBase+"/instagram/media?page=1&per_page=100&period=recent&geo=true&tags[]="+newFilter.tema+"";
     startMap(url,"instagram_map");
 
