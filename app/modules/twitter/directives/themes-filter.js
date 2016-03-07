e06dbee1a82fc963fb564a09b784082dd8665625
Gustavo RPS <email@gustavorps.net>
2016-03-07 14:13:23 -0300

    implemented new theme filter directive of twitter

--- app/modules/twitter/directives/themes-filter.js
+++ app/modules/twitter/directives/themes-filter.js
@@ -13,10 +13,21 @@
           filter: '='
         },
         link: function(scope, element, attrs) {
-          scope.setFilter = function(option, e) {
+          scope.setFilter = function(option, e, main) {
             scope.filter = option.tag;
-            $(element).find('li').removeClass('selected');
+            $('li', element).removeClass('selected');
             $(e.currentTarget).parent().addClass('selected');
+            $(e.currentTarget)
+              .parent()
+              .parent()
+              .parent('.menu__main-tags-option').addClass('selected');
+            $(e.currentTarget)
+              .parent()
+              .parent('.menu__main-tags-options-children').css('display: none;');
+
+            main
+              ? $('.menu__main-tags-current', element).text('Todos')
+              : $('.menu__main-tags-current', element).text(option.title);
           }
         }
       };
