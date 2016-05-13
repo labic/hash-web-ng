0782df2ddae61a1fb3970d8c8536955324c69e25
zoaldyck <roberto_dmoraes@hotmail.com>
2016-05-13 16:03:50 -0300

    Diretiva e imagem para trocar as imagens que são 404 NOT FOUND por uma imagem padrão.

---
+++ app/modules/instagram/directives/error-images.js
@@ -0,0 +1,17 @@
+//(function () {
+//  'use strict';
+//
+//  angular
+//    .module('hash.mainInstagram')
+//    .directive('onErrorSrc', function() {
+//    return {
+//      link: function(scope, element, attrs) {
+//        element.bind('error', function() {
+//          if (attrs.src != attrs.onErrorSrc) {
+//            attrs.$set('src', attrs.onErrorSrc);
+//          }
+//        });
+//      }
+//    }
+//  });
+//})();
