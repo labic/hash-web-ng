35310d7425e5e725347cab66986fd55b54009860
zoaldyck <roberto_dmoraes@hotmail.com>
2016-08-31 16:19:38 -0300

    css/officialStyle.css -  Corrigindo CSS. css/palavra.css - Corrigindo alguns posicionamentos. data/words.config.json - Atualizando grupos. instagram/module.js - Trocando nome do module.js instagram/views/main.html - Alterando a estrutura do Instagram (IMAGES)

--- app/modules/instagram/module.js
+++ app/modules/instagram/module.js
@@ -2,6 +2,6 @@
   'use strict';
   angular.module('hash.instagram', ['uiGmapgoogle-maps'])
   .run(function (settings) {
-    settings.setFromFile('instagram.filters', '/data/instagram.config.json');
+    settings.setFromFile('instagram.filters', '/data/images.config.json');
   });
 })();
