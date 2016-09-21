a57d228d9751c8169d1b29770ae4ac01bad70656
zoaldyck <roberto_dmoraes@hotmail.com>
2016-09-21 16:54:22 -0300

    words/routes.js: Exclusão de rotas não usadas mais.
    app/data/images.config.json: Atualização da data das imagens e mudança dos tempos.
    
    app/data/words.config.json: Atualização da data das words e mudança dos tempos.

--- app/modules/words/routes.js
+++ app/modules/words/routes.js
@@ -8,9 +8,6 @@
       url: '/palavras',
       views: {
         '': { templateUrl: 'modules/words/views/main.html' },
-        //            'sunburst@palavra': { templateUrl: 'modules/words/views/sunburstPalavra.html' },
-        //            'words@palavra': { templateUrl: 'modules/words/views/wordsPalavra.html' },
-        //            'conteudo.tweet@palavra': { templateUrl: 'modules/words/views/twPalavra.html' }
       }
     });
   });
