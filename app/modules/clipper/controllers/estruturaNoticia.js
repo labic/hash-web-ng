74cca55ba464f86d7298e24387b1ff9ebd08679b
bih-goncalves <bih_goncalves@hotmail.com>
2017-09-11 17:17:47 -0300

    inserindo visualização de noticias em outra página

--- app/modules/clipper/controllers/estruturaNoticia.js
+++ app/modules/clipper/controllers/estruturaNoticia.js
@@ -1,20 +1,16 @@
-angular
-    .module('hash.clipper')
+angular.module('hash.clipper')
     .controller("estruturaNoticia", function($scope, $http, $location) { 
-		$scope.url = 'https://inep-hash-data-api-dev.herokuapp.com/articles';
+		var query = $location.search();
+		$scope.url = 'https://inep-hash-data-api-dev.herokuapp.com/articles/'+query.id;
 			    //acessando o JSON
 			    $http({
 			        url: $scope.url,
 			        method:'GET'
-			        //falta parametro de pegar objeto por id
+
 			    })
 			    .then(function (response) {
+			    	$scope.noticia = response.data.data;
 
-			       //tratar o objeto aqui para carregar conteúdo na página
-			       // document.getElementById('modalTitle').innerHTML = info.headline;
-			       // document.getElementById('modalBody').innerHTML = info.articleBody;
-			       // document.getElementById('modalFooter').innerHTML = '<a href="'+info.url+'" target="_blank">Ir para a notícia</a>';
-		        
 			    },
 			    function (err) {
 			        console.log(err);
