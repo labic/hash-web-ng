fb4f9b52ef20e2a783cfa7f7f2b0425a9ad7d590
bih-goncalves <bih_goncalves@hotmail.com>
2017-09-13 19:51:34 -0300

    utilizando link interno para notícias

--- app/modules/clipper/controllers/estruturaNoticia.js
+++ app/modules/clipper/controllers/estruturaNoticia.js
@@ -1,19 +1,22 @@
 angular.module('hash.clipper')
-    .controller("estruturaNoticia", function($scope, $http, $location) { 
+    .controller("estruturaNoticia", function($scope, $http, $location,$sce) { 
 		var query = $location.search();
 		$scope.url = 'https://inep-hash-data-api-dev.herokuapp.com/articles/'+query.id;
-			    //acessando o JSON
-			    $http({
-			        url: $scope.url,
-			        method:'GET'
+	    //acessando o JSON
+	    $http({
+	        url: $scope.url,
+	        method:'GET'
 
-			    })
-			    .then(function (response) {
-			    	$scope.noticia = response.data.data;
+	    })
+	    .then(function (response) {
+	    	$scope.noticia = response.data.data;
+	    	$scope.site = $scope.noticia.url.split('/')[2];
+	    	$scope.html = $sce.trustAsHtml($scope.noticia.articleBody);
 
-			    },
-			    function (err) {
-			        console.log(err);
-			    });
+	    },
+	    function (err) {
+	        console.log("Notícia não encontrada");
+	        document.getElementById('conteudo').innerHTML ="<h1><b>Erro ao carregar conteúdo</b></h1><br><br><a href='#/clipper'>Voltar</a>";
+	    });
 
 });
