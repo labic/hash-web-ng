052baaa1646985834b04c9e35b431689f9472097
Grégory Derner <gregoryderner@gmail.com>
2017-11-02 17:25:59 -0200

    fix(wordCloud.js): Update bower para nova versão metrics.

--- app/js/wordCloud.js
+++ app/js/wordCloud.js
@@ -12,7 +12,7 @@ function plotWordCloud(options, wordMap) {
   padding = options.padding || 0;
 
   var term = wordMap[0].word ? 'word': 'hashtag';
-  var sizeScale = d3.scale.linear().domain([0, wordMap[0].count]).range([15, 60]);
+  var sizeScale = d3.scaleLinear().domain([0, wordMap[0].count]).range([15, 60]);
 
   d3.layout.cloud().size([w, h])
     .words(wordMap.map(function (d, i) {
