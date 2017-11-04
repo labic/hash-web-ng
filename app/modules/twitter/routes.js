7390b2a2ddba02f64e0a840b4b627f79f082a9c0
bih-goncalves <bih_goncalves@hotmail.com>
2017-11-04 19:01:09 -0200

    invertendo ordem de carregamento de dados iniciais

--- app/modules/twitter/routes.js
+++ app/modules/twitter/routes.js
@@ -11,7 +11,7 @@
           '': { templateUrl: 'modules/twitter/views/main.html' },
           'map@twitter': { templateUrl: 'modules/twitter/views/map.html' },
           'word&Tag@twitter': { templateUrl: 'modules/twitter/views/wordTweet.html' },
-          'conteudo@twitter': { templateUrl: 'modules/twitter/views/twTweet.html' }
+          'conteudo@twitter': { templateUrl: 'modules/twitter/views/twTweetsTweets.html' }
         }
       })
       .state('twitter.word', {
