33723fe354d2bb8dbcbadc53ce31283472cfdc75
willianlopes404 <willianlopes404@gmail.com>
2017-11-05 12:25:34 -0200

    ajustes na legenda das estatísticas

--- app/js/graphs.js
+++ app/js/graphs.js
@@ -6,40 +6,40 @@ function plotGraphics(divID, data){
 	.attr("id",function(d,i){return "mg-graphic" + i})
 	.attr("class","mg-stats-div")
 	.style("margin-left","10px")
-	.style("margin-right","10px")	
+	.style("margin-right","10px")
 
 	for(i in data){
-		plot("mg-graphic" + i,data[i]);	
-	}	
+		plot("mg-graphic" + i,data[i]);
+	}
 }
 
-function plot(divID, data){		
+function plot(divID, data){
 	MG.data_graphic({
 	  title: data.rede.toUpperCase() + " - " + data.param,
 	  description: "",
-	  data: data.data,	  	  
+	  data: data.data,
 	  full_width:true,
 	  height: 200,
 	  target: "#" + divID,
 	  x_accessor: "date",
-	  y_accessor: "value",	  
-	  color: '#004D85',	
-	  xax_count: data.data.length,	  
+	  y_accessor: "value",
+	  color: '#004D85',
+	  xax_count: data.data.length,
 	});
 }
 
 function trataEntrada(res, network){
 	data = []
 
-	for(i in res){		
+	for(i in res){
 		data.push({rede:network, param:res[i].label, data:convert(res[i].data)});
 	}
-	
+
 
 	return data;
 }
 
-function convert(json){	
+function convert(json){
 	vet = []
 	for (i in json){
 		vet.push({"date":convertData(i),value:json[i]})
@@ -49,33 +49,33 @@ function convert(json){
 
 function convertData(data){
 	temp = data.split("T");
-	temp2 = temp[0].split("-");	
+	temp2 = temp[0].split("-");
 	return new Date(temp2[2] + "-" + temp2[1] + "-" + temp2[0] + "T" + temp[1])
 }
 
-function plotMultiLineGraph(divID,vet, tipo){	
+function plotMultiLineGraph(divID,vet, tipo){
 	newData = [];
 	legends = [];
 	title = vet[0].rede + " - " + tipo;
 	for(i in vet){
-		newData[i] = MG.clone(vet[i].data);		
+		newData[i] = MG.clone(vet[i].data);
 		legends[i] = vet[i].param
 	}
 
 	console.log(newData);
 	console.log(legends);
-	
+
 	//plota o gráfico
 	MG.data_graphic({
-	  title: title,	  
-	  data: newData,	  
+	  title: title,
+	  data: newData,
 	  width: width,
 	  height: 200,
 	  right: 70,
-	  target: "#" + divID,	  	  
+	  target: "#" + divID,
 	  xax_format: d3.utcFormat('%d %b (%H:00)'),
 	  color: '#004D85',
-	  legend: legends	  
+	  legend: legends
 	});
 }
 
